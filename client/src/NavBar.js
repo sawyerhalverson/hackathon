7d767e00cc6a80295350841f37ff441317ee93fb
sawyerhalverson <shalverson321@gmail.com>
2024-01-26 13:16:16 -0700

    ok

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -4,12 +4,18 @@ export default function NavBar(){
     return <nav className="nav">
         <a href="/" className="site-title">Site Name</a>
         <ul>
-            <li>
-                <a href="/pricing">Pricing</a>
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
+            <CustomLink href="/pricing">Pricing</CustomLink>
+            <CustomLink href="/about">About</CustomLink>
         </ul>
     </nav>
+}
+
+
+function CustomLink({href, children, ...props}){
+    const path = window.location.pathname
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href} {...props}>{children}</a>
+        </li>
+    )
 }
