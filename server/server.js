cfd95b9c13de4ca4a880ec0910471b87e8620227
sawyerhalverson <shalverson321@gmail.com>
2024-01-26 12:41:43 -0700

    ok

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
-import express from "express";
-import mysql from 'mysql';
-import cors from 'cors';
+
+
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'root123',
+    database: 'hackathon'
+
+});
+
+//select all from database
+app.get('/', (req,res) => {
+    const sql = "Select * from table_name";
+    db.query(sql,(err,data)=>{
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
+
 
 
 app.listen(8081, ()=> {
