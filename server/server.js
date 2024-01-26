26baa5430dbc16723d1d60b78cb6e4832ce54a4a
sawyerhalverson <shalverson321@gmail.com>
2024-01-26 11:54:28 -0700

    ok

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import mysql from 'mysql';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(8081, ()=> {
+    console.log("Listening");
+})
