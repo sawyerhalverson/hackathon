fe489d003865d8e393b4025e705684097bb2e4b0
sawyerhalverson <shalverson321@gmail.com>
2024-01-26 13:10:39 -0700

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import NavBar from './NavBar';
 import './styles.css'
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
 
 
 function App() {
+  let component
+  switch (window.location.pathname){
+    case "/":
+      component = <Home/>
+      break
+    case "/pricing":
+      component = <Pricing/>
+      break
+    case "/about":
+      component = <About/>
+      break
+
+  }
   return (
+    <>
     <NavBar/>
-    // <div className="App">
-    //   Hi
-    //   <BrowserRouter>
-    //     <Routes>
-    //       <Route path="/" element={<Item/>}> </Route> 
-    //       {/* replace Item with something else */}
-    //     </Routes>
-    //   </BrowserRouter>
-    // </div>
-  );
+    <div className='container'>
+      {component}
+    </div>
+    </>
+
+  )
 }
 
 export default App;
