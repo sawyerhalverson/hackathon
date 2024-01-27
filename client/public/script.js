ae1d0db8803e04361fa506412bdd339a14de71f0
sawyerhalverson <shalverson321@gmail.com>
2024-01-27 12:12:29 -0700

    ok

--- client/public/script.js
+++ client/public/script.js
@@ -1,26 +1,45 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2RoYWx2ZXJzb24iLCJhIjoiY2xyd2R2Yzk4MHczODJpbnpwbTFseWF3MiJ9.OFD8OqtsIqk21jGKgV45WA';
 
-
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation,{
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
     enableHighAccuracy: true
-})
+});
 
-function successLocation(position){
-    console.log(position)
-    setupMap([position.coords.longitude, position.coords.latitude])
+function successLocation(position) {
+    console.log(position);
+    setupMap([position.coords.longitude, position.coords.latitude]);
 }
 
-function errorLocation(){}
+function errorLocation() {
+    setupMap([-111.652884, 40.250495]);
+}
 
-function setupMap(center){
-    var map = new mapboxgl.Map({
+function setupMap(center) {
+    const map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
-        center:center
-    
-    })
+        center: center,
+        zoom: 14.5
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+    // Add GeolocateControl
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true,
+            showUserHeading: true
+        })
+    );
+
+    // Add MapboxDirections
+    var directions = new MapboxDirections({
+        accessToken: 'pk.eyJ1Ijoic2RoYWx2ZXJzb24iLCJhIjoiY2xyd2R2Yzk4MHczODJpbnpwbTFseWF3MiJ9.OFD8OqtsIqk21jGKgV45WA',
+        // Add any additional options for MapboxDirections
+    });
+
+    map.addControl(directions, 'top-left');
 }
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-  });
