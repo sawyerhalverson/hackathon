4b9b953e36ab28ed886daba4859e3d7340328199
Collin McGregor <60762138+collinmcgregor@users.noreply.github.com>
2024-01-27 15:57:49 -0700

    added opacity to the map and new background

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -3,29 +3,23 @@ import React, { useState } from 'react';
 const Map = () => {
   const [selectedDestination, setSelectedDestination] = useState('');
 
-  const handleDestinationChange = (event) => {
-    setSelectedDestination(event.target.value);
+  const handleDestinationClick = (destination) => {
+    setSelectedDestination(destination);
   };
 
   return (
     <div>
       <h2>Map</h2>
-      <p>Where do you want to go?</p>
-
-      <label>
-        Select Destination:
-        <select value={selectedDestination} onChange={handleDestinationChange}>
-          <option value="">-- Select --</option>
-          <option value="Tanner Building">Tanner Building</option>
-          <option value="Wilkinson Center">Wilkinson Center</option>
-          <option value="JKB">JKB</option>
-        </select>
-      </label>
+    
+      {/* Include map or any other content related to the selected destination */}
+      <div style={{ opacity: 1 }}>
+        {/* Map content goes here */}
+      </div>
 
       {selectedDestination && (
         <div>
           <p>You selected: {selectedDestination}</p>
-          {/* Include map or any other content related to the selected destination */}
+          {/* Additional details or actions related to the selected destination */}
         </div>
       )}
     </div>
